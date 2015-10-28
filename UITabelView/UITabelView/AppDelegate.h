a96b6638f5cd18c7b20c9e61b6c80d6205b19c76
cew <1636647532@qq.com>
2015-10-28 15:48:22 +0800

    初始化项目

---
+++ UITabelView/UITabelView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITabelView
+//
+//  Created by chen on 15/10/28.
+//  Copyright © 2015年 chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
